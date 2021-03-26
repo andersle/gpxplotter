20d2128fd0bf60cf1b1174cc81560ec79eece359
Anders Lervik <andersle@gmail.com>
2021-03-26 09:23:19 +0100

    Style-update for example

--- docs/source/gallery/plots/plot_006_heart_rate.py
+++ docs/source/gallery/plots/plot_006_heart_rate.py
@@ -15,7 +15,7 @@ import numpy as np
 plt.style.use('seaborn-talk')
 
 
-for track in read_gpx_file('example3.gpx'):
+for track in read_gpx_file('example4.gpx'):
     for i, segment in enumerate(track['segments']):
         time = segment['time']
         time_in_zones = {}
@@ -35,21 +35,28 @@ for track in read_gpx_file('example3.gpx'):
         labels = [f'Zone {i}\n({percent[i]:.1f}%)' for i in zones]
         values = [time_in_zones[i] for i in zones]
         cmap = get_cmap(name='Reds')
-        colors = cmap(np.linspace(0, 1, 6))
-        colors = colors[1:]  # Skip the color for zone = 0
+        colors = cmap(np.linspace(0, 1, len(zones) + 1))
+        colors = colors[1:]  # Skip the first color
         fig, ax1 = plt.subplots(constrained_layout=True)
-        ax1.pie(
+        patches, _ = ax1.pie(
             values,
             colors=colors,
-            labels=labels,
+            # labels=labels,  # Labels may overlap, so this is commented here
+            # textprops={'fontsize': 'x-large', 'ha': 'center'},
             labeldistance=1.15,
             wedgeprops={'width': 0.45, 'linewidth': 3, 'edgecolor': 'w'},
-            textprops={'fontsize': 'x-large', 'ha': 'center'},
             normalize=True,
             startangle=90,
             counterclock=False,
         )
         ax1.set(aspect='equal')
+
+        legend = ax1.legend(patches, labels, loc='upper left',
+                            bbox_to_anchor=(-0.15, 1.),
+                            handlelength=3, fontsize='x-large')
+        # Make patches thicker for the legend:
+        for patch in legend.get_patches():
+            patch.set_height(20)
         ax1.text(
             0, 0, 'Time in\nheart rate zones',
             fontdict={'fontsize': 'x-large', 'ha': 'center', 'va': 'center'},
